2651c452be5bd143f36bc51c6be004303359b432
Somanshu Singla <ssomanshus@gmail.com>
2019-01-14 16:44:36 +0530

    start.spring.io

---
+++ src/main/java/com/somanshu/springframework/spring5framework/Spring5frameworkApplication.java
@@ -0,0 +1,14 @@
+package com.somanshu.springframework.spring5framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5frameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5frameworkApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/somanshu/springframework/spring5framework/Spring5frameworkApplicationTests.java
@@ -0,0 +1,17 @@
+package com.somanshu.springframework.spring5framework;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5frameworkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
